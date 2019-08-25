c34bcd574d5dea84e445b24aeb3a0d3c1f2a0173
mr-h4sh <50665436+mr-h4sh@users.noreply.github.com>
2019-08-25 13:44:06 -0400

    Create oppgave5.c

---
+++ oppgave5.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main() 
+{
+    // DEL A - printe 1960 ut av 19600406 
+    int rar_variabel = 19600406 / 10000; 
+    printf("%d\n", rar_variabel); 
+
+    // beregne og skrive ut volum på rettvinklet prisme hvor sider er 8, 12, 16
+    int a = 8;
+    int b = 12; 
+    int c = 16; 
+
+    int d = a * b * c; 
+    printf("%d\n", d);
+    return 0;
+}
